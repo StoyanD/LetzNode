695f675be13416f56efbfbb36c9c7e4f9bde8f3f
Stoyan <s.dimitrov19@gmail.com>
2014-04-23 17:22:03 -0700

    added favicon

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , favicons = require('connect-favicons')
   , path = require('path');
 
 var app = express();
@@ -16,7 +17,8 @@ app.locals.pretty = true;
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(favicons(__dirname + '/public/images/icons'));
+//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
