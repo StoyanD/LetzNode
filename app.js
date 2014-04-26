e8ceef459bb8469cb3cc88248ff730c2a6effa86
Stoyan <s.dimitrov19@gmail.com>
2014-04-26 15:51:06 -0700

    adding rout of register to app and user.js

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ app.configure(function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.post('/register', user.register);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
