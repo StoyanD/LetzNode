003543165c166b3efe6f8ab6ba5a6b24f8512e85
Stoyan <s.dimitrov19@gmail.com>
2014-04-26 15:24:13 -0700

    added mongoose, and updated configs for dev and production in main app.js

--- app.js
+++ app.js
@@ -8,27 +8,43 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , favicons = require('connect-favicons')
-  , path = require('path');
+  , path = require('path')
+  , mongoose = require('mongoose');
 
 var app = express();
 
-app.locals.pretty = true;
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(favicons(__dirname + '/public/images/icons'));
-//app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var mongoUri = process.env.MONGOLAB_URI || 
+	process.env.MONGOHQ_URL || 'mongodb://localhost/letz';
+//location  local ::   grep dbpath /etc/mongodb.conf
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log('Connection to db established : ' + mongoUri);
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.configure('development', function(){
+	console.log('app in development mode');
+	app.locals.pretty = true;
+	app.use(express.errorHandler());
+	app.use(express.logger('dev'));
+});
+
+app.configure('production', function(){
+	console.log('app in production mode');
+    app.enable('view cache');
+});
+
+app.configure(function(){
+	// all environments
+	app.set('port', process.env.PORT || 3000);//master branch
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
