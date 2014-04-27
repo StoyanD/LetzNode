fc9eb619bc7a9b262431d78a39f7fb03f05636d2
Stoyan <s.dimitrov19@gmail.com>
2014-04-26 18:58:48 -0700

    trying to change app status on facebook to work outside of development

--- public/js/facebook/like_share.js
+++ public/js/facebook/like_share.js
@@ -1,3 +1,27 @@
+ window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '691356804261220',
+          status     : true,
+          xfbml      : true
+        });
+      };
+
+      (function(d, s, id){
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {return;}
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));      
+
+
+
+
+
+
+
+
+
 (function(d, s, id) {
  var js, fjs = d.getElementsByTagName(s)[0];
  if (d.getElementById(id)) return;
