79194d98bd7f2cb666f4b192425de0d74ec0f9de
Stoyan <s.dimitrov19@gmail.com>
2014-04-26 19:22:06 -0700

    trying to get fb to work on production

--- public/js/facebook/like_share.js
+++ public/js/facebook/like_share.js
@@ -1,20 +1,20 @@
- window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '691356804261220',
-          status     : true,
-          xfbml      : true
-        });
-      };
+// window.fbAsyncInit = function() {
+//        FB.init({
+//          appId      : '691356804261220',
+//          status     : true,
+//          xfbml      : true
+//        });
+//      };
+//
+//      (function(d, s, id){
+//          var js, fjs = d.getElementsByTagName(s)[0];
+//          if (d.getElementById(id)) {return;}
+//          js = d.createElement(s); js.id = id;
+//          js.src = "//connect.facebook.net/en_US/all.js";
+//          fjs.parentNode.insertBefore(js, fjs);
+//        }(document, 'script', 'facebook-jssdk'));      
 
-      (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/all.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));      
-
-
+      
 
 
 
@@ -29,3 +29,4 @@
  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=691356804261220";
  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
