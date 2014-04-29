e37f2545d50446cda246b646e5908d724991645c
Stoyan <s.dimitrov19@gmail.com>
2014-04-29 12:42:15 -0700

    testing change of facebook app for like buttons

---
+++ public/js/facebook/like_share_test.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '487104168086034',
+          status     : true,
+          xfbml      : true
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=487104168086034";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
